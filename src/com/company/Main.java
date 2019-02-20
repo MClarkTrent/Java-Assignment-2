0686831ddbb3fa051647b63b2615b738bdff1a61
Matt Clark <MattClark@TFED7.WPA.Dal.Ca>
2019-02-20 13:09:19 -0400

    Added pre built classes from assignment sheet

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,22 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //Create Geometric Object Array
+        GeometricObject gObjectArray [] = new GeometricObject [4];
+        //Complete your code here
+
+
+        System.out.println("welcome");
+        double testPrint = Circle.getCenterX();
+        System.out.println(testPrint);
+
     }
-}
+
+    public static void
+
+    private static void printAreaAndPerimeter(GeometricObject gObject) {
+        //Complete your code here
+
+
+    }//End of Main method
+}//End of Class
